91446d941ecee74aeadaf295f29f350077c163b5
olivia <olivia@fastmail.com>
2018-01-22 14:32:43 +0100

    fix error handling

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ module.exports = async function (middleware, dest = 'dest', opts = {}) {
 
   const files = await promisify(vfileRead.all)(f => {
     return !excludes.some(e => f.path.includes(e))
-  }, source).catch(t(e))
+  }, source).catch(e => t(e))
   files.shift()
   files.map(f => vfile.readSync(f))
 
   const chain = trough()
   middleware.forEach(m => chain.use(m))
-  await promisify(chain.run)(files).catch(t(e))
+  await promisify(chain.run)(files).catch(e => t(e))
 
   files.forEach(async f => {
     const relativePath = path.relative(source, f.path)
     const destPath = path.join(dest, relativePath)
     f.path = destPath
-    await promisify(mkdirp)(f.dirname).catch(t(e))
-    await promisify(vfile.write)(f).catch(t(e))
-  }).catch(t(e))
+    await promisify(mkdirp)(f.dirname).catch(e => t(e))
+    await promisify(vfile.write)(f).catch(e => t(e))
+  }).catch(e => t(e))
 }
 
 function t (e) { throw new Error(e) }
