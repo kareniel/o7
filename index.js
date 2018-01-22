58038f2149cea4d05fcfbc3230c59ed0fedabf67
olivia <olivia@fastmail.com>
2018-01-22 14:44:55 +0100

    refactor error handling once again

--- index.js
+++ index.js
@@ -9,23 +9,31 @@ module.exports = async function (middleware, dest = 'dest', opts = {}) {
   const source = opts.source || process.cwd()
   const excludes = opts.exclude || [dest]
 
-  const files = await promisify(vfileRead.all)(f => {
+  const files = await t(promisify(vfileRead.all), f => {
     return !excludes.some(e => f.path.includes(e))
-  }, source).catch(e => t(e))
+  }, source)
   files.shift()
   files.map(f => vfile.readSync(f))
 
   const chain = trough()
   middleware.forEach(m => chain.use(m))
-  await promisify(chain.run)(files).catch(e => t(e))
+  await t(promisify(chain.run), files)
 
   files.forEach(async f => {
     const relativePath = path.relative(source, f.path)
     const destPath = path.join(dest, relativePath)
     f.path = destPath
-    await promisify(mkdirp)(f.dirname).catch(e => t(e))
-    await promisify(vfile.write)(f).catch(e => t(e))
-  }).catch(e => t(e))
+    await t(promisify(mkdirp), f.dirname)
+    await t(promisify(vfile.write), f)
+  })
 }
 
-function t (e) { throw new Error(e) }
+async function t (fn, ...args) {
+  let res
+  try {
+    res = await fn(...args)
+  } catch (e) {
+    throw new Error(e)
+  }
+  return res
+}
